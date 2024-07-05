0b69b10c05d965758d1f4e6c01b27cf46699153f
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-07-05 13:01:57 +0530

    copy constructor

--- Encap.java
+++ Encap.java
@@ -2,17 +2,33 @@ public class Encap {
     public static void main(String args[]){
 
         Student s1 = new Student("Ratnadeep");
-        System.out.println(s1.name);
+        s1.roll =456;
+        s1.password="abcd";
+        
+        Student s2 = new Student(s1);//copy constructor s1 is copied into s2
+        s2.password="xyz";
     }
 }
 
 class Student{
     String name;
     int roll ;
+    String password;
+    Student(Student s1){
+        //copy constructor
+        this.name = s1.name;
+        this.roll = s1.roll;
+    }
+    Student(){  
+        System.out.println("constructor is called");
+    }
     Student(String name){
         //constructor 
         this.name = name ;
 
+    }
+    Student(int roll){
+        this.roll = roll;
     }
 
 }
