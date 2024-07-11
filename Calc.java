ce3bcaf68533d91fc08a3ebf8d0d29d4ae385a8c
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-07-11 11:43:08 +0530

    feat: Add Dog class and demonstrate method overriding

--- Calc.java
+++ Calc.java
@@ -4,8 +4,22 @@ public class Calc {
         System.out.println(c1.sum(2,3));
         System.out.println(c1.sum(2.5f,3.5f));
         System.out.println(c1.sum(2,3,4));
+
+        Dog Jony = new Dog();
+        Jony.eat();
     }
 }
+ class Animal{
+    void eat(){
+        System.out.println("eating");
+    }
+ }
+ class Dog extends Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+
+ }
 //Method overloading 
  class Calculator{
     int sum( int a,int b){
